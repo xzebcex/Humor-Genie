d8e9e4cc9ef187d811f02727fe70e08eda2c9086
Aman Pandit <amanpandit3027@gmail.com>
2023-01-27 09:57:44 +0530

    Create humor.py

---
+++ humor.py
@@ -0,0 +1,19 @@
+# Humor Genie.
+# This is a random joke generator.
+
+import random
+
+jokes = ["Once my father came home and found me in front of a roaring fire. That made my father very mad, as we didn\'t have a fireplace",
+         "A pair of cows were talking in the field. One says,\"Have you heard about the mad cow disease that's going around?\"\n \"Yeah,\"The other cow says.\"Makes me glad I\'m a penguin",
+         "\"Your mother has been with us for 20 years,\" said John. “Isn't it time she got a place of her own?\n\"“My mother?” replied Helen. “I thought she was your mother.”",
+         "Why don't pirates take a shower before they walk the plank?\nThey just wash up on shore.",
+         "According to unofficial sources, a new simplified income-tax form contains only four lines:\n 1.What was your income for the year?\n 2.What were your expenses?\n 3.How much have you left?\n 4.Send it in.",
+         "One of the oddities of Wall Street is that it is the dealer and not the customer who is called broker.",
+         "Two Hollywood stars ran into each other at the door of their psychiatrist's office.\n\n“Hello, there,” said one. “Are you coming or going?”\n\n \"If I knew that,\" said the other, “I wouldn't be here.\"",
+         "At a party, a young wife admonished her husband, \"That's the fourth time you've gone back for ice cream and cake. Doesn't it embarrass you?\n\"Why should it?\" answered her spouse. \"I keep telling them it's for you.\"",
+         "A teenager brings her new boyfriend home to meet her parents. They're appalled by his haircut, his tattoos, his piercings\nLater, the girl's mom says,\"Dear, he doesn't seem to be a very nice boy.\n\"Oh, please, Mom!\"says the daughter. \"If he wasn't nice, would he be doing 500 hours of community service?",
+         "Every time a little boy went to a playmate's house, he found the friend's grandmother deeply engrossed in her Bible. Finally his curiosity got the better of him.\n\"Why do you suppose your grandmother reads the Bible so much?\" he asked\n\"I'm not sure,\" said his friend, \"but I think she's cramming for her finals.\""
+         ]
+
+
+print(random.choice(jokes))
